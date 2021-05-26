d25660f227b36effc17cdaee0d58d280d2686c73
Gideon Etim <gidiblack@gmail.com>
2021-05-26 19:10:57 +0100

    add recipe form setup

--- src/components/recipes/RecipesPage.tsx
+++ src/components/recipes/RecipesPage.tsx
@@ -1,7 +1,48 @@
 import React from "react";
 
-function RecipesPage(): JSX.Element {
-  return <h2>Recipes</h2>;
+interface IState {
+  recipe: {
+    title: string;
+  };
+}
+
+// interface IProps {
+//   title: string;
+// }
+
+class RecipesPage extends React.Component<IState> {
+  state = {
+    recipe: {
+      title: "",
+    },
+  };
+  // use arrow function to decare method because they inherit the binding context of their enclosing scope
+  handleChange = (e: any) => {
+    // copy state using object spread syntax then overwrite the title property by setting it to the target.value passed in on the event
+    const recipe = { ...this.state.recipe, title: e.target.value };
+    // set the state to the new recipe object
+    this.setState({ recipe: recipe });
+  };
+
+  handleSubmit = (e: any) => {
+    e.preventDefault();
+    alert(this.state.recipe.title);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Recipes</h2>
+        <h3>Add Recipes</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.recipe.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
+  }
 }
 
 export default RecipesPage;
