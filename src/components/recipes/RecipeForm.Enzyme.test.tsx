884f2184738854a064c03c51b839d896874dbe66
Gideon Etim <gidiblack@gmail.com>
2021-06-20 19:34:15 +0100

    enzyme shallow testing

--- src/components/recipes/RecipeForm.Enzyme.test.tsx
+++ src/components/recipes/RecipeForm.Enzyme.test.tsx
@@ -5,7 +5,7 @@ import RecipeForm from "./RecipeForm";
 
 configure({ adapter: new Adapter() }); // configure configures the Adapter
 
-function renderRecipeForm(args: any) {
+function renderRecipeForm(args?: any) {
   const defaultProps = {
     authors: [],
     recipe: {},
@@ -18,3 +18,19 @@ function renderRecipeForm(args: any) {
   const props = { ...defaultProps, ...args };
   return shallow(<RecipeForm {...props} />);
 }
+
+it("renders form and header", () => {
+  const wrapper = renderRecipeForm();
+  expect(wrapper.find("form").length).toBe(1); // enzyme's find method accepts css selectors (e.g 'form', '.btn', '#id')
+  expect(wrapper.find("h2").text()).toEqual("Add Recipe");
+});
+
+it("Labels save button as 'Save' when not saving", () => {
+  const wrapper = renderRecipeForm();
+  expect(wrapper.find("button").text()).toBe("Save");
+});
+
+it("Labels save button as 'Saving...' when saving", () => {
+  const wrapper = renderRecipeForm({ saving: true });
+  expect(wrapper.find("button").text()).toBe("Saving...");
+});
--- src/components/recipes/RecipeForm.Snapshot.test.tsx
+++ src/components/recipes/RecipeForm.Snapshot.test.tsx
@@ -1,38 +1,43 @@
 import React from "react";
 import RecipeForm from "./RecipeForm";
 import renderer from "react-test-renderer";
-import { recipeData, authorData } from "./../../../utils/mockData";
+
+function renderRecipeForm(args?: any) {
+  // object to hold our default values
+  const defaultProps = {
+    authors: [],
+    recipe: {},
+    errors: {},
+    saving: false,
+    onSave: () => {},
+    onChange: () => {},
+  };
+
+  const props = { ...defaultProps, ...args };
+  // call renderer to render the component. renderer returns a tree which is an object that represents the ouput of our component
+  return renderer.create(<RecipeForm {...props} />);
+}
 
 it("sets submit button label to 'Saving...' when saving is true", () => {
-  // call renderer to render the component. renderer returns a tree which is an object that represents the ouput of our component
   // jest.fn() adds an empty function to the test
-  const tree = renderer.create(
-    <RecipeForm
-      recipe={recipeData[0]}
-      authors={authorData}
-      onSave={jest.fn()}
-      onChange={jest.fn()}
-      saving
-      errors
-    />
-  );
+  // const tree = renderer.create(
+  //   <RecipeForm
+  //     recipe={recipeData[0]}
+  //     authors={authorData}
+  //     onSave={jest.fn()}
+  //     onChange={jest.fn()}
+  //     saving
+  //     errors
+  //   />
+  // );
+
+  const tree = renderRecipeForm({ saving: true });
 
   expect(tree).toMatchSnapshot();
 });
 
 it("sets submit button label to 'Save' when saving is false", () => {
-  // call renderer to render the component. renderer returns a tree which is an object that represents the ouput of our component
-  // jest.fn() adds an empty function to the test
-  const tree = renderer.create(
-    <RecipeForm
-      recipe={recipeData[1]}
-      authors={authorData}
-      onSave={jest.fn()}
-      onChange={jest.fn()}
-      saving={false}
-      errors
-    />
-  );
+  const tree = renderRecipeForm();
 
   expect(tree).toMatchSnapshot();
 });
